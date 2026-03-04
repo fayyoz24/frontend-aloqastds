660d42a0bb162fa54caa37bba896b3a54ef5d301
fayyoz24 <fayyozusmon@gmail.com>
2026-03-04 14:56:33 +0500

    added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",  // Django server
+        target: "https://aloqabankstudents.pythonanywhere.com/",  // Django server
         changeOrigin: true,
       },
     },
