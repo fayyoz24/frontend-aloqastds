1d0db3afc5721f9092f37c6432312cadaa11d173
fayyoz24 <fayyozusmon@gmail.com>
2026-03-04 14:23:35 +0500

    added

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/students/",
+  baseURL: "https://aloqabankstudents.pythonanywhere.com/api/students/",
 });
 
 export default API;
